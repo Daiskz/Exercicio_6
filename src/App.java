afdb63e705f397984efbbf8d22f9cfc2d35e7b0a
gabriel.matsubara@gmail.com <gabriel.matsubara@gmail.com>
2023-08-11 15:42:37 -0300

    Mudanças no projeto

--- src/App.java
+++ src/App.java
@@ -4,11 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         int raio;
         double pi = 3.14, área;
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Digite o raio do círculo: ");
-            raio = sc.nextInt();
-        }
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o raio do círculo: ");
+        raio = entrada.nextInt();
         área = pi * raio * raio;
         System.out.println("A área do círculo é: " + área);
+        entrada.close();
     }
 }
